c628e9c13a4c21bfd74fac563338da35e4c05585
Denis <d.pleshivtsev1@gmail.com>
2022-06-17 18:31:10 +0500

    prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:camera/camera.dart';
-import 'package:master_eye/screens/cam_screen.dart';
 import 'package:master_eye/screens/first_screen.dart';
 import 'package:master_eye/screens/main_screen_phone.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+List<CameraDescription> cameras = [];
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/screens/main_screen_phone.dart
+++ lib/screens/main_screen_phone.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:master_eye/main.dart';
-import 'package:master_eye/screens/cam_screen.dart';
+import 'face_detector_view.dart';
 
 
 class MainScreenPhone extends StatelessWidget {
@@ -15,95 +15,18 @@ class MainScreenPhone extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/logo.png',
-                    width: 125,
-                    height: 60,
-                    fit: BoxFit.fitWidth,
-                  ),
-                ],
-              ),
-            ),
             Row(
               mainAxisSize: MainAxisSize.max,
               children: [
                 Expanded(
                   child: Container(
                     alignment: Alignment.center,
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height * 0.67,
-                    child: CameraScreen()
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height*0.9,
+                    child: FaceDetectorView()
                   ),
                 ),
               ],
-            ),
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 20),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
-                    child: RaisedButton(
-                      onPressed: () {
-                        showDialog(context: context,
-                            builder: (dialog) => AlertDialog(
-                              title: const Text("Ой..."),
-                              content: const Text("Функция недоступна. Следите за обновлениями!"),
-                              elevation: 20.0,
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text(
-                                    'OK',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ));
-                      },
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
-                      padding: const EdgeInsets.all(0.0),
-                      elevation: 3,
-                      child: Ink(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color(0xFF3FBBFE),
-                              Color(0xFFA541FF),
-                            ],
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                        ),
-                        child: Container(
-                          constraints: const BoxConstraints(maxWidth: 325.0, maxHeight: 75.0),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            'Меню',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ) ,
-                  ),
-                ],
-              ),
             ),
           ],
         ),
